0f87066bdf3f2445d1b3a7a703e8abf353189fc6
dylan-sequera <dylan.sequera@iest.edu.mx>
2026-03-05 11:08:42 -0600

    se hizo la tarea

--- Practica3/Practica3/Form1.Designer.vb
+++ Practica3/Practica3/Form1.Designer.vb
@@ -35,6 +35,22 @@ Partial Class Form1
         Me.resultado = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.materia6 = New System.Windows.Forms.TextBox()
+        Me.materia8 = New System.Windows.Forms.TextBox()
+        Me.materia7 = New System.Windows.Forms.TextBox()
+        Me.materia9 = New System.Windows.Forms.TextBox()
+        Me.materia12 = New System.Windows.Forms.TextBox()
+        Me.materia10 = New System.Windows.Forms.TextBox()
+        Me.materia11 = New System.Windows.Forms.TextBox()
+        Me.materia13 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'Label1
@@ -145,11 +161,155 @@ Partial Class Form1
         Me.Button1.Text = "Calcular promedio"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(37, 274)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(112, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Formación Humana IV"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(37, 243)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(45, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Física II"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(37, 219)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(103, 13)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "Educación Física IV"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(37, 189)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(55, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "Biología II"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(37, 363)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(224, 13)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "Taller de Habilidades para el Mundo Laboral II"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(37, 335)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(76, 13)
+        Me.Label12.TabIndex = 18
+        Me.Label12.Text = "Paraescolar IV"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(37, 300)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(60, 13)
+        Me.Label13.TabIndex = 19
+        Me.Label13.Text = "Literatura II"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(37, 391)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(127, 13)
+        Me.Label14.TabIndex = 20
+        Me.Label14.Text = "Taller de Comunicación II"
+        '
+        'materia6
+        '
+        Me.materia6.Location = New System.Drawing.Point(309, 189)
+        Me.materia6.Name = "materia6"
+        Me.materia6.Size = New System.Drawing.Size(100, 20)
+        Me.materia6.TabIndex = 21
+        '
+        'materia8
+        '
+        Me.materia8.Location = New System.Drawing.Point(309, 247)
+        Me.materia8.Name = "materia8"
+        Me.materia8.Size = New System.Drawing.Size(100, 20)
+        Me.materia8.TabIndex = 22
+        '
+        'materia7
+        '
+        Me.materia7.Location = New System.Drawing.Point(309, 221)
+        Me.materia7.Name = "materia7"
+        Me.materia7.Size = New System.Drawing.Size(100, 20)
+        Me.materia7.TabIndex = 23
+        '
+        'materia9
+        '
+        Me.materia9.Location = New System.Drawing.Point(309, 271)
+        Me.materia9.Name = "materia9"
+        Me.materia9.Size = New System.Drawing.Size(100, 20)
+        Me.materia9.TabIndex = 24
+        '
+        'materia12
+        '
+        Me.materia12.Location = New System.Drawing.Point(309, 363)
+        Me.materia12.Name = "materia12"
+        Me.materia12.Size = New System.Drawing.Size(100, 20)
+        Me.materia12.TabIndex = 25
+        '
+        'materia10
+        '
+        Me.materia10.Location = New System.Drawing.Point(309, 300)
+        Me.materia10.Name = "materia10"
+        Me.materia10.Size = New System.Drawing.Size(100, 20)
+        Me.materia10.TabIndex = 26
+        '
+        'materia11
+        '
+        Me.materia11.Location = New System.Drawing.Point(309, 332)
+        Me.materia11.Name = "materia11"
+        Me.materia11.Size = New System.Drawing.Size(100, 20)
+        Me.materia11.TabIndex = 27
+        '
+        'materia13
+        '
+        Me.materia13.Location = New System.Drawing.Point(309, 391)
+        Me.materia13.Name = "materia13"
+        Me.materia13.Size = New System.Drawing.Size(100, 20)
+        Me.materia13.TabIndex = 28
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.materia13)
+        Me.Controls.Add(Me.materia11)
+        Me.Controls.Add(Me.materia10)
+        Me.Controls.Add(Me.materia12)
+        Me.Controls.Add(Me.materia9)
+        Me.Controls.Add(Me.materia7)
+        Me.Controls.Add(Me.materia8)
+        Me.Controls.Add(Me.materia6)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.resultado)
@@ -183,4 +343,20 @@ Partial Class Form1
     Friend WithEvents resultado As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents materia6 As TextBox
+    Friend WithEvents materia8 As TextBox
+    Friend WithEvents materia7 As TextBox
+    Friend WithEvents materia9 As TextBox
+    Friend WithEvents materia12 As TextBox
+    Friend WithEvents materia10 As TextBox
+    Friend WithEvents materia11 As TextBox
+    Friend WithEvents materia13 As TextBox
 End Class
